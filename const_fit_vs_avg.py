2d52d8318699003cc57caa9494ba4afcb0936903
Greyyy-HJC <hejinchen17@gmail.com>
2021-10-27 15:24:52 +0800

    start_today

---
+++ const_fit_vs_avg.py
@@ -0,0 +1,23 @@
+# %%
+############### it shows that constant fit with lsqfit will give different mean value from averaging ##################
+
+import gvar as gv
+import numpy as np
+import lsqfit as lsf
+
+def fcn(x, p):
+    return p['a'] + 0 * x
+
+priors = gv.BufferDict()
+priors['a'] = gv.gvar(0, 10)
+
+x = np.arange(5)
+mean = [0.8, 1, 1, 1.2, 1.4]
+sdev = [0.2, 0.3, 0.2, 0.5, 0.4]
+y = [gv.gvar(mean[i], sdev[i]) for i in range(5)]
+
+fit_result = lsf.nonlinear_fit(data=(x, y), prior=priors, fcn=fcn, maxit=10000, svdcut=1e-100, fitter='scipy_least_squares')
+
+print(fit_result.format(100))
+print(np.average(mean))
+# %%
